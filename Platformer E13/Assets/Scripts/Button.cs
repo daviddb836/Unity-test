8508787b5ecdf07643ba8a25eebf09575b388c2d
David\PC <daviddb836@gmail.com>
2016-10-19 16:59:08 +0200

    test

---
+++ Platformer E13/Assets/Scripts/Button.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+	public Color defaultColour;
+	public Color selectedColour;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	void onTouchDown() {
+		GetComponent<Renderer> ().material.color = Color.red;
+		}
+
+	void onTouchUp() {
+		GetComponent<Renderer> ().material.color = Color.red;
+	}
+	void onTouchStay() {
+		GetComponent<Renderer> ().material.color = Color.red;
+	}
+	void onTouchExit() {
+		GetComponent<Renderer> ().material.color = Color.red;
+	}
+
+}
+		
+
+
